3a8d86b7db6774349883395d2db87b0387e0b621
Melanie Downing <sayanything830@gmail.com>
2017-12-13 12:42:41 -0800

    changed smacss organization

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -2,6 +2,7 @@
 //change to IIFE
 var app = app || {};
 var __API_URL__ = 'http://localhost:3000';
+// var __API_URL__ = 'https://md-hn-booklist.herokuapp.com/';// for deployed testing
 
 (function (module) {
   function errorCallBack(err) {
