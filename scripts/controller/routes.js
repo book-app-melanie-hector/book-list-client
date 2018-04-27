aee2fec05b8f481da448263b9ab74d1698f0e9b6
Melanie Downing <sayanything830@gmail.com>
2018-04-27 09:37:16 -0700

    debugging deployed app

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,31 +1,33 @@
 'use strict';
 
-if(window.location.pathname !== '/') {
-  page.base('/book-list-client');
-}
+// if(window.location.pathname !== '/') {
+//   page.base('/book-list-client');
+// }
+
+if(window.location.host.indexOf('github.io') !== -1) page.base('/book-list-client');
 
 page('/',
   (ctx, next) => app.Book.fetchAll(() => app.bookView.initIndexPage(ctx, next)),
   (ctx, next) => app.adminView.verify(ctx, next)
 );
-page('/book-list-client/books/new',
+page('/books/new',
   ctx => app.bookView.initNewBookPage(ctx)
 );
-page('/book-list-client/books/search',
+page('/books/search',
   ctx => app.bookView.initSearchFormPage()
 );
-page('/book-list-client/books/:book_id/update',
+page('/:book_id/update',
   (ctx, next) => app.Book.fetchOne(ctx, next),
   ctx => app.bookView.initUpdateFormPage(ctx)
 );
-page('/book-list-client/books/:book_id',
+page('/books/:book_id',
   (ctx) => app.Book.fetchOne(ctx, () => app.bookView.initDetailPage(ctx))
 );
 // page('/books/:book_id/update',
 //   (ctx, next) => app.Book.fetchOne(ctx, next),
 // );
 
-page('/book-list-client/admin',
+page('/admin',
   () => app.adminView.initAdminPage()
 );
 page();
